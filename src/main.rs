ff335992fe832d9e049d6581d342f26a90254ed9
David Huss <dh@widerstandzwecklos.at>
2018-12-13 16:15:31 +0100

    Added Colors

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ extern crate toml;
 extern crate bender_job;
 extern crate bender_mq;
 extern crate docopt;
+extern crate colored;
 
 use std::fs;
+use colored::*;
 use std::process;
 use std::path::{PathBuf, Path};
 use app_dirs::*;
@@ -82,7 +84,7 @@ fn main(){
     if args.cmd_get && args.cmd_configpath{
         // Print just the path of the application folder
         match get_app_root(AppDataType::UserConfig, &APP_INFO){
-            Err(err) => eprintln!(" ✖ Error: : Couldn't get application folder: {}", err),
+            Err(err) => eprintln!("{}", format!(" ✖ Error: : Couldn't get application folder: {}", err).red()),
             Ok(app_savepath) => {
                 let mut p = app_savepath.clone();
                 p.push("config.toml");
@@ -92,13 +94,13 @@ fn main(){
     // Read the config (if there is one) and get the path for frames
     }else if args.cmd_get && args.cmd_outpath{
         match get_app_root(AppDataType::UserConfig, &APP_INFO){
-            Err(err) => eprintln!(" ✖ Error: {}", err),
+            Err(err) => eprintln!("{}", format!(" ✖ Error: {}", err).red()),
             Ok(app_savepath) => {
                 let mut configpath = app_savepath.clone();
                 configpath.push("config.toml");
                 match Config::from_file(&configpath){
                     Ok(config) => println!("{}", config.outpath.to_string_lossy()),
-                    Err(err) => eprintln!(" ✖ Error: {}", err)
+                    Err(err) => eprintln!("{}", format!(" ✖ Error: {}", err).red())
                 }
             }
         }
@@ -111,7 +113,7 @@ fn main(){
                 configpath.push("config.toml");
                 match Config::from_file(&configpath){
                     Ok(config) => println!("{}", config.blendpath.to_string_lossy()),
-                    Err(err) => eprintln!(" ✖ Error: {}", err)
+                    Err(err) => eprintln!("{}", format!(" ✖ Error: {}", err).red())
                 }
             }
         }
@@ -124,7 +126,7 @@ fn main(){
                 configpath.push("config.toml");
                 match Config::from_file(&configpath){
                     Ok(config) => println!("{}", config.id),
-                    Err(err) => eprintln!(" ✖ Error: {}", err)
+                    Err(err) => eprintln!("{}", format!(" ✖ Error: {}", err).red())
                 }
             }
         }
@@ -137,7 +139,7 @@ fn main(){
                 configpath.push("config.toml");
                 match Config::from_file(&configpath){
                     Ok(config) => println!("{}", config.bender_url),
-                    Err(err) => eprintln!(" ✖ Error: {}", err)
+                    Err(err) => eprintln!("{}", format!(" ✖ Error: {}", err).red())
                 }
             }
         }
@@ -155,13 +157,13 @@ fn main(){
                                 let p = config.blendpath.to_string_lossy().to_string();
                                 match fs::remove_dir_all(&p){
                                     Ok(_) => {
-                                        println!(" ✔ Deleted the contents of {}", p);
+                                        println!("{}", format!(" ✔ Deleted the contents of {}", p).green());
                                         match fs::create_dir_all(&p){
                                             Ok(_) => (),
-                                            Err(err) => eprintln!(" ✖ Error: Couldn't recreate directory: {}", err)
+                                            Err(err) => eprintln!("{}", format!(" ✖ Error: Couldn't recreate directory: {}", err).red())
                                         }
                                     },
-                                    Err(err) => eprintln!(" ✖ Error while deleting in {}: {}", p, err)
+                                    Err(err) => eprintln!("{}", format!(" ✖ Error while deleting in {}: {}", p, err).red())
                                 }
                             }
 
@@ -169,55 +171,55 @@ fn main(){
                                 let p = config.outpath.to_string_lossy().to_string();
                                 match fs::remove_dir_all(&p){
                                     Ok(_) => {
-                                        println!(" ✔ Deleted the contents of {}", p);
+                                        println!("{}", format!(" ✔ Deleted the contents of {}", p).green());
                                         match fs::create_dir_all(&p){
                                             Ok(_) => (),
-                                            Err(err) => eprintln!(" ✖ Error: Couldn't recreate directory: {}", err)
+                                            Err(err) => eprintln!("{}", format!(" ✖ Error: Couldn't recreate directory: {}", err).red())
                                         }
                                     },
-                                    Err(err) => eprintln!(" ✖ Error while deleting in {}: {}", p, err)
+                                    Err(err) => eprintln!("{}", format!(" ✖ Error while deleting in {}: {}", p, err).red())
                                 }
                             }
                         }else{
                             if args.cmd_blendfiles || (!args.cmd_blendfiles && !args.cmd_frames) {
                                 let p = config.blendpath.to_string_lossy().to_string();
-                                let msg = format!("Delete all files at {} ?", p);
+                                let msg = format!("{}", format!("Delete all files at {} ?", p).on_bright_red());
                                 if Confirmation::new().with_text(&msg).interact().unwrap(){
                                     match fs::remove_dir_all(&p){
                                         Ok(_) => {
-                                            println!(" ✔ Deleted the contents of {}", p);
+                                            println!("{}", format!(" ✔ Deleted the contents of {}", p).green());
                                             match fs::create_dir_all(&p){
                                                 Ok(_) => (),
-                                                Err(err) => eprintln!(" ✖ Error: Couldn't recreate directory: {}", err)
+                                                Err(err) => eprintln!("{}", format!(" ✖ Error: Couldn't recreate directory: {}", err).red())
                                             }
                                         },
-                                        Err(err) => eprintln!(" ✖ Error while deleting in {}: {}", p, err)
+                                        Err(err) => eprintln!("{}", format!(" ✖ Error while deleting in {}: {}", p, err).red())
                                     }
                                 }
                             }
 
                             if args.cmd_frames || (!args.cmd_blendfiles && !args.cmd_frames) {
                                 let p = config.outpath.to_string_lossy().to_string();
-                                let msg = format!("Delete all files at {} ?", p);
+                                let msg = format!("{}", format!("Delete all files at {} ?", p).on_bright_red());
                                 if Confirmation::new().with_text(&msg).interact().unwrap(){
                                     match fs::remove_dir_all(&p){
                                         Ok(_) => {
-                                            println!(" ✔ Deleted the contents of {}", p);
+                                            println!("{}", format!(" ✔ Deleted the contents of {}", p).green());
                                             match fs::create_dir_all(&p){
                                                 Ok(_) => (),
-                                                Err(err) => eprintln!(" ✖ Error: Couldn't recreate directory: {}", err)
+                                                Err(err) => eprintln!("{}", format!(" ✖ Error: Couldn't recreate directory: {}", err).red())
                                             }
                                         },
-                                        Err(err) => eprintln!(" ✖ Error while deleting in {}: {}", p, err)
+                                        Err(err) => eprintln!("{}", format!(" ✖ Error while deleting in {}: {}", p, err).red())
                                     }
                                 }
                             }
                             
                         }
                     },
-                    Err(err) => eprintln!("Error: there was no config.toml at {path}: {err}", 
+                    Err(err) => eprintln!("{}", format!("Error: there was no config.toml at {path}: {err}", 
                         path=&app_savepath.to_string_lossy(),
-                        err=err)
+                        err=err).red())
                 }
             }
         }
@@ -231,24 +233,24 @@ fn run(args: &Args) {
     // Get a valid application save path depending on the OS
     println!("\n{x} BENDER-WORKER {x}", x="=".repeat(24));
     match get_app_root(AppDataType::UserConfig, &APP_INFO){
-        Err(err) => println!("ERROR: Couldn't get application folder: {}", err),
+        Err(err) => println!("{}", format!(" ✖ Error: Couldn't get application folder: {}", err).red()),
         Ok(app_savepath) => {
-            println!("Storing Application Data in:        {}", app_savepath.to_string_lossy().replace("\"", ""));
+            println!("Storing Application Data in:        {}", app_savepath.to_string_lossy().replace("\"", "").bold());
 
             // Load the configuration (or generate one if we are a first timer)
             match config::get_config(&app_savepath, &args){
                 Err(err) => {
                     let e = format!("{}", err);
                     if !e.contains("missing field"){
-                        println!("ERROR: Couldn't generate/read config file: {}", err);
+                        println!("{}", format!(" ✖ Error: Couldn't generate/read config file: {}", err).red());
                     }else{
-                        println!("ERROR: The existing configuration misses a field: {}", err);
-                        let msg = "Do you want to generate a new one? (this overrides the existing configuration)";
-                        if Confirmation::new().with_text(msg).interact().unwrap(){
+                        println!("{}", format!(" ✖ Error: The existing configuration misses a field: {}", err).red());
+                        let msg = "Do you want to generate a new one? (this overrides the existing configuration)".on_bright_red();
+                        if Confirmation::new().with_text(&msg).interact().unwrap(){
                             let mut p = PathBuf::from(&app_savepath);
                             p.push("config.toml");
                             fs::remove_file(&p).expect(format!("Error: Couldn't remove the file at {}\nPlease try to remove it manually", p.to_string_lossy()).as_str());
-                            println!("Deleted the configuration file. Run worker again for a fresh new start");
+                            println!("{}", "Deleted the configuration file. Run worker again for a fresh new start".on_green());
                         }
                     }
 
@@ -264,13 +266,13 @@ fn run(args: &Args) {
                     let url = "amqp://localhost//".to_string();
                     println!("Listening on for AMQP traffic at:   {}", url);
                     println!("Storing jobs at:                    {}", config.blendpath.to_string_lossy());
-                    let mut channel = Channel::open_default_channel().expect(" ✖ [WORKER] Error: Couldn't aquire channel");
+                    let mut channel = Channel::open_default_channel().expect(&format!("{}", format!(" ✖ [WORKER] Error: Couldn't aquire channel").red()));
 
                     // Declare a Work exchange
-                    channel.create_work_queue().expect(" ✖ [WORKER] Error: Declaration of work queue failed");
+                    channel.create_work_queue().expect(&format!("{}", format!(" ✖ [WORKER] Error: Declaration of work queue failed").red()));
 
                     // Declare a topic exchange
-                    channel.declare_topic_exchange().expect(" ✖ [WORKER] Error: Declaration of topic exchange failed");
+                    channel.declare_topic_exchange().expect(&format!("{}", format!(" ✖ [WORKER] Error: Declaration of topic exchange failed").red()));
                     
                     // TODO APPSAVEPATH REINSPEICHERN
 
